aebd7816ac2feccb8b29d34dc670fb86526edccf
carlosloaizaprag <carlos.loaiza@pragmaticingenieria.com>
2025-10-15 19:49:23 -0500

    Mi primer versión api pet_shop con saludo hola campeones

---
+++ src/test/java/co/edu/umanizales/pet_shop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.pet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
